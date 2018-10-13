982afaad08fa03a7a2baff2183e65e249ba280af
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2018-10-13 19:50:19 +0600

    Part 20

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -30,7 +30,7 @@
                                         @csrf
                                         <button type="submit" class="flex items-center focus:outline-none">
                                             <svg class="w-5 h-5 {{ $reply->isFavorited() ? 'text-red' : '' }}" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/></svg>
-                                            <span class="block ml-1 text-base">{{ $reply->favorites()->count() }}</span>
+                                            <span class="block ml-1 text-base">{{ $reply->favorites_count }}</span>
                                         </button>
                                     </form>
                                 </div>
